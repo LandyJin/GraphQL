0ada0a34c630d7e00510de68b96ae89eedd91b65
JIN HUIYAN <huiyanj91@gmail.com>
2019-03-07 16:41:25 +1100

    Setup Apollo

--- ReadMe.md
+++ ReadMe.md
@@ -12,9 +12,29 @@ yarn start
 
 ## Tips to build the app
 
+### SERVER SIDE
+--------
+
 ### express-graphql Setup: 
 
 [express-graphql](https://github.com/graphql/express-graphql)
 
 (server.js, schema.js)
 
+
+### CLIENT SIDE
+--------
+
+### Bootswatch
+
+Bootstrap
+
+[bootswatch](https://bootswatch.com/cyborg/)
+
+### Apollo
+
+[Apollo-Installation](https://www.apollographql.com/docs/react/essentials/get-started.html)
+
+```bash 
+yarn add apollo-boost react-apollo graphql
+```
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.png';
+
+// Import Apollo 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SpaceX</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="logo" />
+        </div>
+      </ApolloProvider>
     );
   }
 }
