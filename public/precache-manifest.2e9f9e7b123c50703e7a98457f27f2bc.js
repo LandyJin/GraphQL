e4ecfc3d82fa77e27f719643904ed0cc354c65cd
JIN HUIYAN <huiyanj91@gmail.com>
2019-03-07 21:53:30 +1100

    add publish folder

---
+++ public/precache-manifest.2e9f9e7b123c50703e7a98457f27f2bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab7fa3f9cb98eae9d88d",
+    "url": "/static/css/main.173c1f25.chunk.css"
+  },
+  {
+    "revision": "ab7fa3f9cb98eae9d88d",
+    "url": "/static/js/main.ab7fa3f9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "91a0de7a1145029c2d11",
+    "url": "/static/js/2.91a0de7a.chunk.js"
+  },
+  {
+    "revision": "5c7b6a98647a49286e0c6ca3aecaf82a",
+    "url": "/static/media/logo.5c7b6a98.png"
+  },
+  {
+    "revision": "6b150e0bb1551f6987680a2302549aec",
+    "url": "/index.html"
+  }
+];
